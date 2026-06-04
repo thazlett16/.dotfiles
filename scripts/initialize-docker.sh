8af4da6732438c903195da51658cf05f14212289
Tim Hazlett <47572900+thazlett16@users.noreply.github.com>
2026-06-04 08:37:01 -0500

    Update initialize-docker.sh

--- scripts/initialize-docker.sh
+++ scripts/initialize-docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-systemctl start docker.service
+# https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
 
+# https://docs.docker.com/engine/install/linux-postinstall/#configure-docker-to-start-on-boot-with-systemd
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
