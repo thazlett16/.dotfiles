c33c64f8c55e4fef61d0819a8219044e35dba50a
Tim Hazlett <timhazlett2016@gmail.com>
2026-08-10 14:32:45 -0500

    Runescape init stuff

--- scripts/initialize-runescape.sh
+++ scripts/initialize-runescape.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 
 flatpak install flathub com.adamcake.Bolt
+
+flatpak override --user --show com.adamcake.Bolt
+
+flatpak override --user --reset com.adamcake.Bolt
+
+flatpak override --user --show com.adamcake.Bolt
