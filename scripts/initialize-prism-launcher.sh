07aa7225f9fb068f9314d63346efa7ec5b293d70
Tim Hazlett <timhazlett2016@gmail.com>
2026-08-16 14:10:26 -0500

    Add prism launcher

---
+++ scripts/initialize-prism-launcher.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+flatpak install flathub org.prismlauncher.PrismLauncher
+
+flatpak override --user --show org.prismlauncher.PrismLauncher
+
+flatpak override --user --reset org.prismlauncher.PrismLauncher
+
+flatpak override --user --show org.prismlauncher.PrismLauncher
