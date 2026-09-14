2ab0ac0bec37c0bac8b8d18dd3b878fc5127f3c5
Tim Hazlett <timhazlett2016@gmail.com>
2026-09-14 11:55:13 -0500

    moving the autocmd to the other file

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
+-- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+vim.api.nvim_create_autocmd("UIEnter", {
+	callback = function()
+		vim.o.clipboard = "unnamedplus"
+	end,
+})
--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -20,11 +20,3 @@ vim.o.confirm = true
 
 -- Always reserve one column
 vim.o.signcolumn = "yes"
-
--- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
-vim.api.nvim_create_autocmd("UIEnter", {
-	callback = function()
-		vim.o.clipboard = "unnamedplus"
-	end,
-})
